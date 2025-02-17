81116724a6ef8340c4f5d88ecdafaeec33d86491
vuongquangminh120802@gmail.com <vuongquangminh120802@gmail.com>
2025-02-17 23:28:20 +0700

    middleware để parse dữ liệu từ queryParam và request body

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const path = require("path")
+const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const { engine } = require("express-handlebars");
@@ -6,28 +6,43 @@ const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+
+// chỗ này chính là middleware được tích hợp sẵn trong express để có thể lấy được biến trong res.body
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
 
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template Engine
-app.engine('.hbs', engine({
-  extname: '.hbs'
-}));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.engine(
+  ".hbs",
+  engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
 
 app.get("/", (req, res) => {
-  res.render('home');
+  res.render("home");
 });
 app.get("/news", (req, res) => {
-  res.render('news');
+  res.render("news");
 });
 
 app.get("/search", (req, res) => {
-  res.render('search');
+  res.render("search");
 });
+
+app.post("/search", (req, res) => {
+  console.log(req.body.q);
+  res.send("123123");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
