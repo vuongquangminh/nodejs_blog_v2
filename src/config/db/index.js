bea4abaee36d1e4d5d025945284158cdc7522ddb
vuongquangminh120802@gmail.com <vuongquangminh120802@gmail.com>
2025-03-03 21:52:19 +0700

    upgrade connect db

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,8 +3,9 @@ const mongoose = require("mongoose");
 async function connect() {
   try {
     await mongoose.connect("mongodb://localhost:27017/f8_education_dev", {
-        useNewUrlParser: true, 
-        useUnifiedTopology: true
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+      // useCreateIndex: true
     });
 
     console.log("connect success");
